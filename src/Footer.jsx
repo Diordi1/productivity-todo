6f6a9340901eb6fb43c8a752f527bc377a13a242
wasteapp111@gmail.com <Diordi1>
2024-12-30 01:37:36 +0530

    "media"

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,48 +2,48 @@ import React from 'react';
 
 function Footer() {
     return ( <div className="container">
-        <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+        <footer className="row row-cols-1 row-cols-sm-3 row-cols-md-5 py-5 my-5 border-top">
           <div className="col mb-3">
             <a href="/" className="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
               <svg className="bi me-2" width="40" height="32"><use ></use></svg>
             </a>
-            <p className="text-body-secondary">© 2024</p>
+            <p className="text-body-secondary text-center">© 2024</p>
           </div>
       
           <div className="col mb-3">
       
           </div>
       
-          <div className="col mb-3">
+          <div className="col mb-3 col-sm-12 col-4">
             <h5>Section</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+            <ul className="nav flex-column col-sm-12 col-6">
+              <li className="nav-item mb-2 col-sm-12 col-6 "><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
+              <li className="nav-item mb-2 col-sm-12 col-6"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
+              <li className="nav-item mb-2 col-sm-12 col-6"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
+              <li className="nav-item mb-2 col-sm-12 col-6"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
+              <li className="nav-item mb-2 col-sm-12 col-6"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
             </ul>
           </div>
       
-          <div className="col mb-3">
+          <div className="col mb-3 col-sm-12 col-4">
             <h5>Section</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+            <ul className="nav flex-column col-sm-12 col-6">
+              <li className="nav-item mb-2 col-sm-12 col-6"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
+              <li className="nav-item mb-2 col-sm-12 col-6"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
+              <li className="nav-item mb-2 col-sm-12 col-6"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
+              <li className="nav-item mb-2 col-sm-12 col-6"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
+              <li className="nav-item mb-2 col-sm-12 col-6"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
             </ul>
           </div>
       
-          <div className="col mb-3">
+          <div className="col mb-3 col-sm-12 col-4">
             <h5>Section</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+            <ul className="nav flex-column col-sm-12 col-6">
+              <li className="nav-item mb-2 col-sm-12 col-6"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
+              <li className="nav-item mb-2 col-sm-12 col-6"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
+              <li className="nav-item mb-2 col-sm-12 col-6"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
+              <li className="nav-item mb-2 col-sm-12 col-6"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
+              <li className="nav-item mb-2 col-sm-12 col-6"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
             </ul>
           </div>
         </footer>
